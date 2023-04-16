71d41bdba8667afc1e844f505cadee548d6819db
mateusseiboth <mateusseiboth@gmail.com>
2023-04-16 10:30:43 -0400

    Adiconada funções de create e update

--- src/services/clientes/ClientesService.js
+++ src/services/clientes/ClientesService.js
@@ -15,7 +15,48 @@ const getClients = async () => {
     } catch (error) {
         console.error(error);
     }
+};
+
+const createCliente= async (nome, telefone, cpf) => {
+    try {
+      const response = await ClientsService.post(
+        "/client/new",
+        {
+            "nome": nome,
+            "cpf": cpf,
+            "telefone": telefone,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${loadToken()}`,
+          },
+        }
+      );
+      return response;
+    } catch (error) {
+      return error.response; 
+    }
+  };
+
+const updateCliente = async (id, nome, cpf, telefone) => {
+    try {
+        const response = await ClientsService.put('/client/update',
+        {
+            "id": id,
+            "nome": nome,
+            "cpf": cpf,
+            "telefone": telefone,
+        },
+        {
+        headers: {
+            Authorization: `Bearer ${loadToken()}`,
+        },
+        });
+        return response;
+    } catch (error) {
+        console.error(error);
+    }
 }
 
-export {getClients};
+export {getClients, updateCliente, createCliente};
 export default ClientsService;
