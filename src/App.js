1191c3d33ee589da65bc7a20badea824ece1e0d7
mateusseiboth <mateusseiboth@gmail.com>
2023-04-15 20:40:31 -0400

    Correção no local da pasta

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 //import Usuarios from "./pages/Usuarios.js";
 import Carros from "./pages/Carros.js";
 import Index from "./pages/inicio.js";
-import Login from "./utils/Login.js";
+import Login from "./pages/Login.js";
 import Logout from "./utils/Logout.js";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
