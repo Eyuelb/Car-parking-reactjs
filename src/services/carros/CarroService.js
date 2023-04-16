bcff2f342418a38fb098245ab84bca4f2cde5784
mateusseiboth <mateusseiboth@gmail.com>
2023-04-16 09:26:58 -0400

    Adição de arquivo com consumo da API para carro

---
+++ src/services/carros/CarroService.js
@@ -0,0 +1,62 @@
+import axios from "axios";
+import { loadToken } from "../../utils/Auth";
+
+const CarrosService = axios.create({
+  baseURL: "http://localhost:3000/api/v1",
+});
+const getCarros = async () => {
+  try {
+    const response = await CarrosService.get("/car/list", {
+      headers: {
+        Authorization: `Bearer ${loadToken()}`,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const createCarro = async (placa, cliente) => {
+  try {
+    const response = await CarrosService.post(
+      "/car/new",
+      {
+        "placa": placa,
+        "cliente_id": cliente,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${loadToken()}`,
+        },
+      }
+    );
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const updateCarro = async (id, placa, cliente_id) => {
+    try {
+      const response = await CarrosService.put(
+        "/car/update",
+        {
+          "placa": placa,
+          "id": id,
+          "cliente_id": cliente_id,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${loadToken()}`,
+          },
+        }
+      );
+      return response;
+    } catch (error) {
+        return error.response;
+    }
+  };
+
+export { getCarros, createCarro, updateCarro };
+export default CarrosService;
